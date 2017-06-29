359ae7aad0303352f5a0765b069447c6a253c00a
kbanico <kbanico@kbanicos-MacBook-Air.local>
2017-06-29 20:08:09 +1000

    students added a shovel, worked on random generation of sprites

--- js/states/level2.js
+++ js/states/level2.js
@@ -5,18 +5,32 @@ var Level2 = {
     },
     preload:function(){
         game.load.image("wackacrab","assets/wackacrab.png");
+        game.load.image("shovel","assets/shovel.png")
     },
     create:function(){
        
         this.background = game.add.sprite(0,0,"wackacrab")
         this.background.height = game.height
         this.background.width = game.width
-        this.troy = game.add.sprite(210,380, "crab")    
-        this.troy.scale.setTo(0.7)
-        this.raeIsDukduk = game.add.sprite(500, 380, "crab")
-        this.raeIsDukduk.scale.setTo(0.7)
+//        this.troy = game.add.sprite(210,380, "crab")    
+//        this.troy.scale.setTo(0.7)
+//        this.raeIsDukduk = game.add.sprite(500, 380, "crab")
+//        this.raeIsDukduk.scale.setTo(0.7)
+//        
+        this.positions = [{x:210,y:150},{x:500,y:150},{x:210,y:280},{x:210,y:380},{x:500,y:280},{x:500,y:380}]
+//        this.positions.forEach(function(potato){
+//            var rasp = game.add.sprite(potato.x, potato.y,"crab");
+//            rasp.scale.setTo(0.7)
+//        },this);
         
-        this.positions = [[210,150],[500, 150],[210,280],[210,380],[500,280],[500,380]]
+        // random genration
+        var randomPopUp = game.rnd.integerInRange(0,5)
+        console.log(randomPopUp);
+        game.add.sprite(this.positions[randomPopUp].x,this.positions[randomPopUp].y,"crab")
+        this.shovel = game.add.sprite(300,150,"shovel")
+        this.shovel.inputEnabled = true;
+        this.shovel.input
+    
     },
     update:function(){
         

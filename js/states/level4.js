fbc31c059dc24dde12a61bb95cb09b425da62639
kbanico <kbanico@kbanicos-MacBook-Air.local>
2017-07-11 18:48:07 +1000

    add callbacks when tween is over

--- js/states/level4.js
+++ js/states/level4.js
@@ -49,6 +49,19 @@ Level4 = {
 //            tween2.to({x:game.rnd.integerInRange(0,game.width-(this.spider.width / 2)),y:100},randomTime)
 //            tween2.start();
 //        }
+        
+        
+        //check the count of alive huts
+        this.countAlive = 0
+        this.huts.forEach(function(e){
+            if(!e.alive){
+                this.countAlive++
+            }
+        },this)
+//        if(this.countAlive == this.huts.length){
+//            game.time.events.add(3000,function(){game.state.restart();})
+//        }
+    
 
     },
     
@@ -57,7 +70,7 @@ Level4 = {
         if(this.spider.alive){
             this.chance = game.rnd.integerInRange(1,10)
             var tween1 = this.game.add.tween(this.spider)
-            if(this.chance > 1 && this.huts.forEachAlive.length != 0){
+            if(this.chance > 0 && this.getRandomFirstAlive(this.huts) != undefined){
                 
 
                 this.house = this.getRandomFirstAlive(this.huts)
@@ -67,14 +80,27 @@ Level4 = {
                 tween1.to({x:this.xPos,y:this.yPos},game.rnd.integerInRange(1,3) * 1000)
                 tween1.start()
                 tween1.onComplete.addOnce(function(){
-                    
-                    this.house.x = game.rnd.integerInRange(this.spider.x - 300, this.spider.x -500)
-                    this.house.y = this.spider.y - 300
-                    this.spider.addChild(this.house)
+                    var newHouse = game.add.sprite(this.house.x,this.house.y,"hut");
+                    this.house.kill();
+                    newHouse.scale.setTo(0.1)
+                    newHouse.x = game.rnd.integerInRange(this.spider.x-450, this.spider.x -200)
+                    newHouse.y = this.spider.y - 600
+                    this.spider.addChild(newHouse)
                     tween1.to({y:0}).start();
+                    tween1.onComplete.addOnce(this.performTween,this)
                    
                 },this)
-            }else{
+            }else if(this.countAlive == this.huts.length){
+                tween3 = game.add.tween(this.spider)
+                tween3.to({x:game.world.centerX,y:game.world.centerY}).start()
+                tween3 = game.add.tween(this.spider.scale).to({x:2,y:2}).start();
+                tween3.onComplete.addOnce(function(){
+                    game.time.events.add(3000,function(){game.state.restart();})
+                },this)
+                
+                console.log("You lose no more house")
+            }
+            else{
                 tween2 = this.game.add.tween(this.spider)
                 for(var i = 0; i < 3; i++){
                     console.log(i)
@@ -83,6 +109,7 @@ Level4 = {
                     tween2.to({x:game.rnd.integerInRange(0,game.width-(this.spider.width / 2)),y:100},randomTime)
                 }
                 tween2.start();
+                tween2.onComplete.addOnce(this.performTween,this)
                 
             }
         }
@@ -90,8 +117,12 @@ Level4 = {
     },
        getRandomFirstAlive:function(group){
         var arr = [];
-        group.forEachAlive(function(element){
-            arr.push(element);
+        group.children.forEach(function(element){
+            console.log("in the get random functin " + element)
+            if(element.alive){
+                arr.push(element);
+            }
+            
         },this)
            
         /*//randomize it
@@ -102,9 +133,10 @@ Level4 = {
             arr[random] = temp
         }*/
         //console.log(arr[0].x)
-        random = Math.floor(Math.random() * group.length)
+        random = Math.floor(Math.random() * arr.length)
         return arr[random]
     },
+
     render:function(){
         this.game.debug.body(this.house1)
         this.game.debug.body(this.house2)

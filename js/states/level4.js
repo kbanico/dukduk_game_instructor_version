1ac605d514932373b98b64c5eba69ee07cd0e9a6
kbanico <kbanico@kbanicos-MacBook-Air.local>
2017-07-11 17:26:13 +1000

    addrandom tweens

--- js/states/level4.js
+++ js/states/level4.js
@@ -33,62 +33,69 @@ Level4 = {
         this.huts.enableBody = true;
         this.huts.setAll("body.allowGravity",false)
         
+        this.performTween()
+        this.getRandomFirstAlive(this.huts)
+
         
-        var tween1 = this.game.add.tween(this.spider)
-        this.chance = game.rnd.integerInRange(1,10)
-        this.yPos = 0
-        if(this.chance != 1){
-            this.chanceHouse = game.rnd.integerInRange(0,2)
-            this.home = this.huts.children[this.chanceHouse]
-            console.log(this.home)
-            this.yPos = this.home.y
-            this.xPos = this.home.x
-            console.log(this.xPos + " is x pos" + this.yPos + " is y pos")
-        }else{
-            this.yPos = this.game.rnd.integerInRange(50,200)
-            this.xPos = this.game.width
-        }
-        tween1.to({x:this.xPos,y:this.yPos},2000)
-        //tween1.start();
-                   
-        console.log(this.chance)
-        
-        tween2 = this.game.add.tween(this.spider)
-        
-//        for(var i = 0; i < 5; i++){
-//            var randomTime = Math.floor(Math.random() * 3) * 1000
+    },update:function(){
+//        if(this.spider.alive){
+//             var randomTime = Math.floor(Math.random() * 3) * Math.floor(Math.random()) *1000
+//             console.log(randomTime)
 ////            tween2.to({y:game.rnd.integerInRange(50,400)},randomTime)
 ////            tween2.to({x:game.width-(this.spider.width / 2)})
 ////            tween2.to({x:game.rnd.integerInRange(0,game.width-(this.spider.width / 2)),y:100})
 //            tween2.to({y:game.rnd.integerInRange(50,400)},randomTime)
 //            tween2.to({x:game.rnd.integerInRange(0,game.width-(this.spider.width / 2))},randomTime)
 //            tween2.to({x:game.rnd.integerInRange(0,game.width-(this.spider.width / 2)),y:100},randomTime)
-//            console.log("finihed" + i)
+//            tween2.start();
 //        }
-//        tween2.start();
-  
-        
-//        var tween1 = this.game.add.tween(this.spider)
-//        tween1.to({x:2800,y: 2600},1000).onComplete.add(function(){    console.log("onComplete");    tween1.to({x:2800,y:this.y +100},1000);    tween1.start();});    tween1.start();
-        
 
-        
-    },update:function(){
+    },
+    
+    performTween:function(){
+        var randomTime = Math.floor(Math.random() * 3) * Math.floor(Math.random()) *1000
         if(this.spider.alive){
-             var randomTime = Math.floor(Math.random() * 3) * Math.floor(Math.random()) *1000
-             console.log(randomTime)
-//            tween2.to({y:game.rnd.integerInRange(50,400)},randomTime)
-//            tween2.to({x:game.width-(this.spider.width / 2)})
-//            tween2.to({x:game.rnd.integerInRange(0,game.width-(this.spider.width / 2)),y:100})
-            tween2.to({y:game.rnd.integerInRange(50,400)},randomTime)
-            tween2.to({x:game.rnd.integerInRange(0,game.width-(this.spider.width / 2))},randomTime)
-            tween2.to({x:game.rnd.integerInRange(0,game.width-(this.spider.width / 2)),y:100},randomTime)
-            tween2.start()
+            this.chance = game.rnd.integerInRange(1,10)
+            var tween1 = this.game.add.tween(this.spider)
+            if(this.chance > 1 ){
+                
+                this.chanceHouse = game.rnd.integerInRange(0,2)
+                this.house = this.huts.children[this.chanceHouse]
+                this.yPos = this.house.y;
+                this.xPos = this.house.x
+                tween1.to({x:this.xPos,y:this.yPos},game.rnd.integerInRange(1,3) * 1000)
+                console.log("should start and get at house " + this.chanceHouse + " the house is " + this.huts.children[this.chanceHouse].x + " and y is " + this.huts.children[this.chanceHouse].y )
+                tween1.start()
+            }else{
+                tween2 = this.game.add.tween(this.spider)
+                for(var i = 0; i < 3; i++){
+                    console.log(i)
+                    tween2.to({y:game.rnd.integerInRange(50,400)},randomTime)
+                    tween2.to({x:game.rnd.integerInRange(0,game.width-(this.spider.width / 2))},randomTime)
+                    tween2.to({x:game.rnd.integerInRange(0,game.width-(this.spider.width / 2)),y:100},randomTime)
+                }
+                tween2.start();
+            }
         }
-
+            
+    },
+       getRandomFirstAlive:function(group){
+        var arr = [];
+        group.forEachAlive(function(element){
+            arr.push(element);
+        },this)
+        for(var i = arr.length - 1; i > 0; i--){
+            random = Math.floor(Math.random() * arr.length)
+            temp = arr[i]
+            arr[i] = arr[random]
+            arr[random] = temp
+        }
+        console.log(arr[0].x)
+        return arr
     },
     render:function(){
         this.game.debug.body(this.house1)
+        this.game.debug.body(this.house2)
         this.game.debug.body(this.spider)
 
 }
